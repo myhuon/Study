34292fcdfc311f8d47361341c14b0772d04aed49
hyunsooSong <myhuon3321@gmail.com>
2022-05-21 01:00:19 +0900

    Update case_expression.sql

--- SQLD/case_expression.sql
+++ SQLD/case_expression.sql
@@ -1,3 +1,10 @@
+/*
+  CASE(시작)
+    WHEN 조건식 THEN (조건식 true일 때수행문) // simplify => column WHEN 조건 THEN true수행문
+    ESLSE (조건식 false일 때 수행문)
+  END(종료)
+*/
+
 --SEARCHED_CASE_EXPRESSION
 SELECT LOC
   CASE WHEN LOC = 'NEW YORK' THEN 'EAST'
