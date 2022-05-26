092b0d7d5eba19112590556666714a1f5d359570
hyunsooSong <myhuon3321@gmail.com>
2022-05-26 14:37:37 +0900

    Add files via upload

---
+++ DataEngineering/spark/lab/Join/Product.java
@@ -0,0 +1,12 @@
+public class Product{
+	String id;	
+	String price;
+	String joinKey;
+
+	public Product(String id, String price, String joinKey){
+		this.id = id;
+		this.price = price;
+		this.joinKey = joinKey;
+	}
+
+}
