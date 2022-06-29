935ca96085644ed0e56205437179ac981d927a00
hyunsooSong <myhuon3321@gmail.com>
2022-06-29 15:17:35 +0900

    Update NavigationMain.swift

--- iOS/Swift/day06/Navigation/NavigationMain.swift
+++ iOS/Swift/day06/Navigation/NavigationMain.swift
@@ -9,39 +9,54 @@
 
 import UIKit
 
-class ViewController: UIViewController, EditDelegate { // EditViewController에 선언한 protocal 구현함.
+class ViewController: UIViewController, EditDelegate, DateDelegate {
+  
+    // EditViewController에 선언한 protocal 구현함.
     
     @IBOutlet var txMessage: UITextField!
     @IBOutlet var imgView: UIImageView!
+    @IBOutlet var lblDate: UILabel!
     
     let imgOn = UIImage(named: "lamp-on.png")
     let imgOff = UIImage(named: "lamp-off.png")
     
     var isOn = true
+    var isZoom = false
+    var isZoomOut = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         imgView.image = imgOn
+        lblDate.text = " "
     }
 
     // 화면 전환 시 자동으로 호출되는 함수 (세그웨이 방향이 정방향일 때 사용)
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segue.destination as! 세그웨이 Identifier
         // as : Typecasting이라고 생각. (목적지를 identifier로 타입 캐스팅 해서 editViewController 변수에 넣음)
-        let editViewController = segue.destination as! EditViewController
-        
-        if segue.identifier == "editButton" {
-            // textWayValue는 멤버 변수임
-            editViewController.textWayValue = "seque : use button"
-        } else if segue.identifier == "editBarButton" {
-            editViewController.textWayValue = "seque : use Bar button"
+        if segue.identifier == "dateButton" {
+            let dateViewController = segue.destination as! DatePickerViewController
+            dateViewController.dateDelegate = self
         }
-        
-        editViewController.textMessage = txMessage.text!
-        editViewController.isOn = isOn
-        editViewController.delegate = self  // delegate 역할은 ViewController로 지정
+        else {
+            let editViewController = segue.destination as! EditViewController
+            
+            if segue.identifier == "editButton" {
+                // textWayValue는 멤버 변수임
+                editViewController.textWayValue = "seque : use button"
+            } else if segue.identifier == "editBarButton" {
+                editViewController.textWayValue = "seque : use Bar button"
+            }
+            
+            editViewController.textMessage = txMessage.text!
+            editViewController.isOn = isOn
+            editViewController.isZoom = isZoom
+            editViewController.isZoomOut = isZoomOut
+            editViewController.delegate = self  // delegate 역할은 ViewController로 지정
+        }
+       
     }
     
     func didMessageEditDone(_ controller: EditViewController, message: String) {
@@ -57,4 +72,39 @@ class ViewController: UIViewController, EditDelegate { // EditViewController에 
             self.isOn = false
         }
     }
+    
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool) {
+        if isZoom {
+            let scale: CGFloat = 2.0 // let은 상수를 선언할 때 사용함
+            var newWidth: CGFloat, newHeight: CGFloat
+            
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            
+            // CGSize는 메모리 고려하지 않고 저장 가능
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            self.isZoom = true
+            self.isZoomOut = false
+        }
+    }
+    
+    func didImageZoomOutDone(_ controller: EditViewController, isZoomOut: Bool) {
+        if isZoomOut {
+            let scale: CGFloat = 2.0 // let은 상수를 선언할 때 사용함
+            var newWidth: CGFloat, newHeight: CGFloat
+            
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            
+            // CGSize는 메모리 고려하지 않고 저장 가능
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            self.isZoomOut = true
+            self.isZoom = false
+        }
+    }
+    
+    func didDatePickDone(_ controller: DatePickerViewController, date: String) {
+        lblDate.text = date
+    }
 }
+
