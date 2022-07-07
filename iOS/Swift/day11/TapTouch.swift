536529549e87f1a56ade2c3562ac6047e826e46a
hyunsooSong <myhuon3321@gmail.com>
2022-07-07 12:26:34 +0900

    Create TapTouch.swift

---
+++ iOS/Swift/day11/TapTouch.swift
@@ -0,0 +1,45 @@
+/*
+  
+  Tap : 화면 클릭 -> 떼기
+  Touch : 화면 클릭 -> 움직임 -> 떼기
+
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtMessage: UILabel!
+    @IBOutlet var txtTapCount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { // Tap은 영향 받지 않는다.
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+
+}
